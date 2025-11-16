62dd5109c470a7b0f1146e61d27c80bb2776fe38
Munem Shahriar <munemshahriar526@gmail.com>
2025-11-16 22:41:49 +0600

    first page update

--- src/main/java/com/example/demo2/HelloController.java
+++ src/main/java/com/example/demo2/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.demo2;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button createButton;
+
 }
